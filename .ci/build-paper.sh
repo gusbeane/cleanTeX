dd971467b43a9438fc957b7b6528d42f4bd95644
Gus Beane <abeane@sas.upenn.edu>
2018-11-06 20:08:39 -0500

    Use docker tectonic

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -3,19 +3,19 @@
 if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'paper/'
 then
   # Install tectonic using conda
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  #wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 
-  bash miniconda.sh -b -p $HOME/miniconda
-  export PATH="$HOME/miniconda/bin:$PATH"
+  #bash miniconda.sh -b -p $HOME/miniconda
+  #export PATH="$HOME/miniconda/bin:$PATH"
 
   # Conda Python
-  hash -r
-  conda config --set always_yes yes --set changeps1 no
-  conda update -q conda
-  conda info -a
-  conda create --yes -n paper
-  source activate paper
-  conda install -c conda-forge -c pkgw-forge tectonic
+  #hash -r
+ # conda config --set always_yes yes --set changeps1 no
+  #conda update -q conda
+  #conda info -a
+  #conda create --yes -n paper
+  #source activate paper
+  #conda install -c conda-forge -c pkgw-forge tectonic
   
   # wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
   # bash miniconda.sh -b -p $HOME/miniconda
